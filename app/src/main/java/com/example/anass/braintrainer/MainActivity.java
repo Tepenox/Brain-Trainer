cf3cfb1fe8b7f75f9fb7aeec3739d009780c92a1
Tepenox <anasselafyabusiness@gmail.com>
2018-11-10 04:48:16 +0100

    populating choices with possible anwsers

--- app/src/main/java/com/example/anass/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/anass/braintrainer/MainActivity.java
@@ -1,13 +1,120 @@
 package com.example.anass.braintrainer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timerTextView;
+    TextView scoreTextView;
+    TextView questionTextView;
+    TextView answerTextView;
+    Button choice1;
+    Button choice2;
+    Button choice3;
+    Button choice4;
+    Button correctChoice;
+    List<Button> choices;
+    int gameState;
+
+    Random rand;
+
+    int randomNum1;
+    int randomNum2;
+    int correctAnswer;
+
+    CountDownTimer gameTime;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timerTextView = findViewById(R.id.timerTextView);
+        scoreTextView = findViewById(R.id.scoreTextView);
+        questionTextView = findViewById(R.id.questionTextView);
+        answerTextView = findViewById(R.id.answerTextView);
+        choice1 = findViewById(R.id.choice1);
+        choice2 = findViewById(R.id.choice2);
+        choice3 = findViewById(R.id.choice3);
+        choice4 = findViewById(R.id.choice4);
+        choices = new ArrayList<>(Arrays.asList(choice1, choice2, choice3,
+                choice4));
+        rand = new Random();
+
+        correctAnswer = generateTwoRandomNumber();
+        String questionString = randomNum1 + " + " + randomNum2 + " = " + "?";
+        questionTextView.setText(questionString);
+        populateChoices();
+
+
+        gameTime = new CountDownTimer(30000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // update the timer
+                String timerText = millisUntilFinished / 1000 + "s";
+                timerTextView.setText(timerText);
+
+
+            }
+
+            @Override
+            public void onFinish() {
+                //endGame()
+            }
+        }.start();
+
+
     }
+
+    private int generateTwoRandomNumber() {
+        //generating two number between 1 and  20 both inclusive
+        //                               max  min      min
+        randomNum1 = rand.nextInt((20 - 1) + 1) + 1;
+        randomNum2 = rand.nextInt((20 - 1) + 1) + 1;
+        return randomNum1 + randomNum2;
+    }
+
+    private void populateChoices() {
+        //choosing randomly one choice to be the correct answer
+        //and others  to be close answers in range of 10;
+        ArrayList<Button> tmpChoices = new ArrayList<>(choices);
+        int randomChoice = rand.nextInt(4);
+        correctChoice = tmpChoices.get(randomChoice);
+        String correctChoiceStr = "" + correctAnswer;
+        correctChoice.setText(correctChoiceStr); //just to avoid some warnings
+        tmpChoices.remove(randomChoice);
+        //iterate thought  the remaining choices and populate them with close values to the correct
+        //answer
+        int min = correctAnswer - 5;
+        int max = correctAnswer + 5;
+
+        for (Button btn : tmpChoices) {
+            int closeAnswer = rand.nextInt((max - min) + 1) + min;
+            //checking if the randomly generated answer is the same as the correct answer
+            //if yes generate a new one until it s not the same
+            while (true){
+                if (closeAnswer == correctAnswer){
+                    closeAnswer = rand.nextInt((max - min) + 1) + min;
+                }else {
+                    break;
+                }
+            }
+            String str = "" + closeAnswer;
+            btn.setText(str);
+        }
+
+
+    }
+
+
 }
